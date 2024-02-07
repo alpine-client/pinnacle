58e9aab8a9f424fb14e55d001f767fecbe8462da
xEricL <ericlatawiec123@gmail.com>
2024-02-06 20:37:47 -0500

    add linux case and fix support pop-up opening every time

--- src/error.go
+++ src/error.go
@@ -41,17 +41,18 @@ func openSupportWebsite() {
 	var err error
 	switch Sys {
 	case Windows:
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", supportURL).Start()
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", SupportURL).Run()
 	case Mac:
-		err = exec.Command("open", supportURL).Start()
+		err = exec.Command("open", SupportURL).Run()
+	case Linux:
+		err = exec.Command("xdg-open", SupportURL).Run()
 	default:
-		// Chances are we are on some form of Linux, this is our best bet
-		err = exec.Command("xdg-open", supportURL).Start()
+		err = errors.New("unable to open support page")
 	}
-	if err == nil {
+	if err != nil {
 		// None of the above worked. Create new popup with url.
 		_ = zenity.Info(
-			fmt.Sprintf("Please visit %s for assistance.", supportURL),
+			fmt.Sprintf("Please visit %s for assistance.", SupportURL),
 			zenity.Title("Error"), zenity.InfoIcon,
 		)
 	}
