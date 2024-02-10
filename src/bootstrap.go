4e7dcc31f99882cb28c6ecb2fdd8899d9ef15bb4
xEricL <ericlatawiec123@gmail.com>
2024-02-09 22:50:49 -0500

    give instructions in error message if we fail to delete JRE

--- src/bootstrap.go
+++ src/bootstrap.go
@@ -109,7 +109,7 @@ func BeginJre(wg *sync.WaitGroup) {
 
 	extractedPath := filepath.Join(basePath, "extracted")
 	err = os.RemoveAll(extractedPath)
-	HandleFatalError("Failed to delete old JRE", err, hub)
+	HandleFatalError("Failed JRE cleanup. Please make sure Alpine Client is not already running.", err, hub)
 	zipArchiver := &archiver.Zip{StripComponents: 1, OverwriteExisting: true}
 	err = zipArchiver.Unarchive(targetPath, extractedPath)
 	HandleFatalError("Failed to unzip JRE", err, hub)
