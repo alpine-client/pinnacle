a136f8018c4d9f24a68d11f5347d3c6131f9612b
xEricL <ericlatawiec123@gmail.com>
2024-04-08 15:46:54 -0400

    fix unnecessarily long defer() in BeginLauncher()

--- src/bootstrap.go
+++ src/bootstrap.go
@@ -18,7 +18,7 @@ const TotalTasks = 13
 
 var CompletedTasks = 0
 
-type metadataResponse struct {
+type MetadataResponse struct {
 	Url  string `json:"url"`
 	Hash string `json:"sha1"`
 	Size uint32 `json:"size"`
@@ -29,66 +29,73 @@ type jreManifest struct {
 	Size uint32 `json:"size"`
 }
 
+func FetchMetadata(url string) *MetadataResponse {
+	hub := CreateSentryHub("FetchMetadata")
+	body, err := GetFromUrl(url)
+	CaptureAndExit(err, hub)
+	defer body.Close()
+
+	var res MetadataResponse
+	err = json.NewDecoder(body).Decode(&res)
+	CaptureAndExit(err, hub)
+	return &res
+}
+
+func FileHashMatches(hash string, path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+	sha := sha1.New()
+	if _, err = io.Copy(sha, file); err != nil {
+		return false
+	}
+	if hex.EncodeToString(sha.Sum(nil)) == hash {
+		return true
+	}
+	return false
+}
+
 func BeginLauncher(wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	hub := CreateSentryHub("BeginLauncher")
+	updateProgress(1)
+	launcher := FetchMetadata(MetadataURL + "/pinnacle")
+	updateProgress(1)
+
 	targetPath := filepath.Join(WorkingDir, "launcher.jar")
-	body, err := GetFromUrl(MetadataURL + "/pinnacle")
-	CaptureAndExit(err, hub)
-	defer body.Close()
-	updateProgress(1)
-
-	var res metadataResponse
-	err = json.NewDecoder(body).Decode(&res)
-	CaptureAndExit(err, hub)
-	updateProgress(1)
-
-	if FileExists(targetPath) {
-		var file *os.File
-		if file, err = os.Open(targetPath); err == nil {
-			defer file.Close()
-			sha := sha1.New()
-			_, _ = io.Copy(sha, file)
-			if hex.EncodeToString(sha.Sum(nil)) == res.Hash {
-				return
-			}
-		}
-
+	if !FileExists(targetPath) || !FileHashMatches(launcher.Hash, targetPath) {
+		updateProgress(1)
+		err := DownloadFromUrl(launcher.Url, targetPath)
+		CaptureAndExit(err, hub)
 	}
 	updateProgress(1)
-	err = DownloadFromUrl(res.Url, targetPath)
-	CaptureAndExit(err, hub)
-	updateProgress(1)
+	return
 }
 
 func BeginJre(wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	hub := CreateSentryHub("BeginJre")
-
 	basePath := filepath.Join(WorkingDir, "jre", "17")
-	manifestPath := filepath.Join(basePath, "version.json")
+
 	err := os.MkdirAll(basePath, os.ModePerm)
 	CaptureAndExit(err, hub)
 	updateProgress(1)
 
-	body, err := GetFromUrl(fmt.Sprintf("%s/jre?version=17&os=%s&arch=%s", MetadataURL, Sys, Arch))
 	CaptureAndExit(err, hub)
-	defer body.Close()
-	updateProgress(1)
+	jre := FetchMetadata(fmt.Sprintf("%s/jre?version=17&os=%s&arch=%s", MetadataURL, Sys, Arch))
+	updateProgress(2)
 
-	var res metadataResponse
-	err = json.NewDecoder(body).Decode(&res)
-	CaptureAndExit(err, hub)
-	updateProgress(1)
-
+	manifestPath := filepath.Join(basePath, "version.json")
 	if FileExists(manifestPath) {
 		var manifest jreManifest
 		var bytes []byte
 		if bytes, err = os.ReadFile(manifestPath); err == nil {
 			if err = json.Unmarshal(bytes, &manifest); err == nil {
-				if manifest.Hash == res.Hash {
+				if manifest.Hash == jre.Hash {
 					updateProgress(5)
 					return
 				}
@@ -98,7 +105,7 @@ func BeginJre(wg *sync.WaitGroup) {
 	updateProgress(1)
 
 	targetPath := filepath.Join(basePath, "jre.zip")
-	err = DownloadFromUrl(res.Url, targetPath)
+	err = DownloadFromUrl(jre.Url, targetPath)
 	CaptureAndExit(err, hub)
 	updateProgress(1)
 
@@ -110,7 +117,7 @@ func BeginJre(wg *sync.WaitGroup) {
 	CaptureAndExit(err, hub)
 	updateProgress(1)
 
-	bytes, err := json.Marshal(jreManifest{Hash: res.Hash, Size: res.Size})
+	bytes, err := json.Marshal(jreManifest{Hash: jre.Hash, Size: jre.Size})
 	CaptureAndExit(err, hub)
 
 	err = os.WriteFile(manifestPath, bytes, os.ModePerm)
