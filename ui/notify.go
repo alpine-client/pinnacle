e3e1dd48520ace01e2104a08f52ddc1d0e024518
xEricL <ericlatawiec123@gmail.com>
2024-05-01 15:00:45 -0400

    remove icon from update notification

--- ui/notify.go
+++ ui/notify.go
@@ -7,8 +7,5 @@ import (
 const downloadURL = "https://alpineclient.com/download"
 
 func NotifyNewUpdate() {
-	_ = zenity.Notify(
-		"New version available!\n\nPlease visit "+downloadURL,
-		zenity.Icon(zenity.InfoIcon),
-	)
+	_ = zenity.Notify("New version available!\n\nPlease visit " + downloadURL)
 }
